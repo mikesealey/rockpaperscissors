7044bd81d5493689d5b5b65ae25fb5827ba24cac
Mike Sealey <39269380+mikesealey@users.noreply.github.com>
2022-10-18 11:41:29 +0100

    include video feature
    Whilst incomplete, this update adds a short WikiHow video tutorial on how RockPaperScissors is played.  There is some underlying code  outlining a feature that swaps the tutorial to either a win or loss video, however more learning is needed to successfully implement this.

--- script2.js
+++ script2.js
@@ -1,17 +1,39 @@
 let userScore = 0
 let computerScore = 0
 
-
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"]
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
+function changeVideo(url) {
+    document.getElementById('ytVideo').src = `${url}`
+    // So this was a silly litle feature that is supposed to change the 
+    // tutorial video on the page in to a different video depending on the outcome
+    // HOWEVER it seems to have issues around certain ad-blocking extensions
+    // and may need to trigger a re-load of the entire video element
+    // which is most likely currently above my pay-grade and far beyond the
+    // scope of the original project
+};
+
 function playRound(userChoice) {
     let computerChoice = getComputerChoice()
     let outcome = ""
 
-    if (userChoice == computerChoice) {  // Draw
+
+    if (userScore > 4) {
+        console.log(`Win condition met, userScore = ${userScore}, computerScore = ${computerScore}`) // Win condition met, Here's why...
+        button.forEach(elem => {elem.disabled = true}) // turn the buttons off
+        outcome = "This was a triumph!"
+        //document.getElementById(ytVideo).src = 'https://www.youtube.com/embed/Y6ljFaKRTrI'
+        changeVideo('https://www.youtube.com/embed/Y6ljFaKRTrI')
+    } else if (computerScore > 4) {
+        console.log(`Win condition met, userScore = ${userScore}, computerScore = ${computerScore}`) // Win condition met, Here's why...
+        button.forEach(elem => {elem.disabled = true}) // turn the buttons off
+        outcome = "You lose! Good day sir!"
+        //document.getElementById('ytVideo').src = 'https://www.youtube.com/embed/M5QGkOGZubQ'
+        changeVideo('https://www.youtube.com/embed/M5QGkOGZubQ')
+    } else if (userChoice == computerChoice) {  // Draw
         outcome = `${userChoice} vs ${computerChoice}, it's a draw!`
         console.log(outcome)
         console.log(userScore)
@@ -40,15 +62,21 @@ function playRound(userChoice) {
         console.log(outcome)
         console.log(userScore)
         console.log(computerScore)
-    } else if (userChoice == "scissors" && computerChoice == "rock") {
+    } else if (userChoice == "scissors" && computerChoice == "rock") { // scissors vs rock
+        outcome = `${userChoice} vs ${computerChoice},  you lose!`
+        computerScore += 1 
+        console.log(outcome)
+        console.log(userScore)
+        console.log(computerScore) 
+    } else if (userChoice == "scissors" && computerChoice == "paper") { // scissors vs paper
         outcome = `${userChoice} vs ${computerChoice},  you win!`
         userScore += 1 
         console.log(outcome)
         console.log(userScore)
         console.log(computerScore)
-    } else if (userChoice == "scissors" && computerChoice == "paper") {
-        outcome = `${userChoice} vs ${computerChoice},  you lose!`
-        computerScore += 1 
+    } else if (userChoice == "shotgun") {
+        outcome = `User chose Shotgun, which is cheating.`
+        userScore += 5
         console.log(outcome)
         console.log(userScore)
         console.log(computerScore)
@@ -57,23 +85,8 @@ function playRound(userChoice) {
     document.getElementById("outcome").innerHTML = outcome
     document.getElementById("userScore").innerHTML = userScore
     document.getElementById("computerScore").innerHTML = computerScore
+
+    
     
 }  // Closing tag of playRound(userChoice)
-
-if (userScore < 5 && computerScore < 5) {
-    const buttons = document.querySelectorAll("input")
-    // Listen to each button, and when clicked, that button runs playRound with that button as the argument
-    buttons.forEach(button => button.addEventListener('click', function(){
-        playRound(button.value)
-    }))
-} else if (userScore == 5) {
-    button.forEach(elem => {elem.disabled = true})
-    document.getElementById("outcome").innerHTML = "You Win!"
-    console.log("win condition met")
-} else if (computerScore == 5) {
-    button.forEach(elem => {elem.disabled = true})
-    document.getElementById("outcome").innerHTML = "Better luck next time!"
-    console.log("win condition met")
-}
-
 
